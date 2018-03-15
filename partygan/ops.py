b9c5f513c528d6a07528c57dd45bfed7f0113fef
Veith Roethlingshoefer <veith21@gmail.com>
2018-03-15 23:37:49 +0100

    Implement conv2d, con2d_transposed, residual block

--- partygan/ops.py
+++ partygan/ops.py
@@ -1,76 +1,148 @@
 import tensorflow as tf
+from tensorflow.python.ops.random_ops import truncated_normal
 import numpy as np
 
-def linear(input_, output_size, scope=None, stddev=1.0, bias_start=0.0, with_w=False):
-    shape = input_.get_shape().as_list()
 
-    with tf.variable_scope(scope or "Linear"):
-        matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
-                                 tf.random_normal_initializer(stddev=stddev))
-        bias = tf.get_variable("bias", [output_size],
-            initializer=tf.constant_initializer(bias_start))
-        if with_w:
-            return tf.matmul(input_, matrix) + bias, matrix, bias
+def conv2d(x, output_channels, scope=None, kernel=(5, 5), activation=None, stride=1):
+
+    strides = [1, stride, stride, 1]
+    in_dim = x.get_shape().as_list()[-1]
+
+    if activation is not None:
+        x = activation(x)
+
+    with tf.variable_scope(scope or "conv2"):
+        initializer = tf.contrib.layers.xavier_initializer(uniform=True, seed=None, dtype=tf.float32)
+        W = tf.get_variable(name="weight", shape=[*kernel, in_dim, output_channels],
+                            dtype=tf.float32, initializer=initializer,
+                            trainable=True)
+
+        b = tf.get_variable(name="bias", shape=[output_channels],
+                            dtype=tf.float32, initializer=tf.constant_initializer(0.),
+                            trainable=True)
+
+        x = tf.nn.conv2d(input=x, filter=W, strides=strides, padding='SAME',
+                         use_cudnn_on_gpu=True,
+                         data_format='NHWC')
+        x = tf.add(x, b)
+
+    return x
+
+
+def conv2d_transposed(x, output_channels, scope=None, kernel=(4, 4), stride=2, activation=None):
+    assert isinstance(output_channels, int)
+    assert isinstance(stride, int)
+
+    channels = max(output_channels, 2)
+    strides = [1, stride, stride, 1]
+    input_channels = x.get_shape()[-1].value
+
+    with tf.variable_scope(scope or "conv2d_transposed"):
+
+        if activation is not None:
+            x = activation(x)
+
+        input_shape = tf.shape(x)
+        h = input_shape[1] * stride
+        w = input_shape[2] * stride
+        output_shape = tf.stack([input_shape[0], h, w, channels])
+
+        bilinear_initializer = bilinear_weight_initializer(filter_size=kernel)
+
+        W = tf.get_variable(name="weight", shape=[*kernel, channels, input_channels],
+                            dtype=tf.float32, initializer=bilinear_initializer,
+                            trainable=True)
+
+        b = tf.get_variable(name="bias", shape=[output_channels],
+                            dtype=tf.float32, initializer=tf.constant_initializer(0.),
+                            trainable=True)
+
+        x = tf.nn.conv2d_transpose(value=x, filter=W, output_shape=output_shape, strides=strides,
+                                   padding='SAME', data_format='NHWC', name="conv2d_transposed")
+
+    return tf.add(x, b)
+
+
+def bilinear_weight_initializer(filter_size, add_noise=True):
+
+    def _initializer(shape, dtype=tf.float32, partition_info=None):
+        if shape:
+            # second last dimension is input, last dimension is output
+            fan_in = float(shape[-2]) if len(shape) > 1 else float(shape[-1])
+            fan_out = float(shape[-1])
         else:
-            return tf.matmul(input_, matrix) + bias
+            fan_in = 1.0
+            fan_out = 1.0
 
-def fully_connected(input_, output_size, scope=None, stddev=1.0, with_bias = True):
-    shape = input_.get_shape().as_list()
+        # define weight matrix (set dtype always to float32)
+        weights = np.zeros((filter_size[0], filter_size[1], int(fan_in), int(fan_out)), dtype=dtype.as_numpy_dtype())
 
-    with tf.variable_scope(scope or "FC"):
-        matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
-            tf.random_normal_initializer(stddev=stddev))
+        # get bilinear kernel
+        bilinear = bilinear_filt(filter_size=filter_size)
+        bilinear = bilinear / fan_out  # normalize by number of channels
 
-        result = tf.matmul(input_, matrix)
+        # set filter in weight matrix (also allow channel mixing)
+        for i in range(weights.shape[2]):
+            for j in range(weights.shape[3]):
+                weights[:, :, i, j] = bilinear
 
-        if with_bias:
-            bias = tf.get_variable("bias", [1, output_size],
-                initializer=tf.random_normal_initializer(stddev=stddev))
-            result += bias*tf.ones([shape[0], 1], dtype=tf.float32)
+        # add small noise for symmetry breaking
+        if add_noise:
+            # define standard deviation so that it is equal to 1/2 of the smallest weight entry
+            std = np.min(bilinear) / 2
+            noise = truncated_normal(shape=shape, mean=0.0,
+                                                              stddev=std, seed=None, dtype=dtype)
+            weights += noise
 
-        return result
+        return weights
 
-def lrelu(x, leak=0.2, name="lrelu"):
-    assert leak < 1 and leak > 0
-    with tf.variable_scope(name):
-        f1 = 0.5 * (1 + leak)
-        f2 = 0.5 * (1 - leak)
-        return f1 * x + f2 * abs(x)
+    return _initializer
 
+def bilinear_filt(filter_size=(4, 4)):
+    assert isinstance(filter_size, (list, tuple)) and len(filter_size) == 2
 
-def conv2d(x, output_dim, kernel_size=(5, 5), stride_diff=(2, 2), stddev=0.02, bias_start=0., scope=None):
-    with tf.variable_scope(scope or "conv2d"):
-        w = tf.get_variable("w", list(kernel_size) + [x.get_shape()[-1]] + [output_dim],
-                            initializer=tf.truncated_normal_initializer(stddev=stddev))
-        conv = tf.nn.conv2d(x, w, strides=(1,) + stride_diff + (1,), padding="SAME")
-        biases = tf.get_variable("biases", [output_dim], initializer=tf.constant_initializer(bias_start))
-        conv = tf.nn.bias_add(conv, biases)
+    factor = [(size + 1) // 2 for size in filter_size]
 
-        return conv
+    if filter_size[0] % 2 == 1:
+        center_x = factor[0] - 1
+    else:
+        center_x = factor[0] - 0.5
 
+    if filter_size[1] % 2 == 1:
+        center_y = factor[1] - 1
+    else:
+        center_y = factor[1] - 0.5
 
-def conv2d_transpose(x, output_dim, kernel_size=(5, 5), stride_diff=(2, 2), stddev=0.02, scope=None, with_w=False):
-    with tf.variable_scope(scope or "conv2d_transpose"):
-        w = tf.get_variable("w", list(kernel_size) + [output_dim[-1]] + [x.get_shape()[-1]],
-                            initializer=tf.random_normal_initializer(stddev=stddev))
+    og = np.ogrid[:filter_size[0], :filter_size[1]]
+    kernel = (1 - abs(og[0] - center_x) / factor[0]) * (1 - abs(og[1] - center_y) / factor[1])
 
-        deconv = tf.nn.conv2d_transpose(x, w, output_shape=output_dim, strides=(1,) + stride_diff + (1,))
-        biases = tf.get_variable("biases", [output_dim[-1]], initializer=tf.constant_initializer(0.))
-        deconv = tf.nn.bias_add(deconv, biases)
+    return kernel
 
-        if with_w:
-            return deconv, w, biases
-        else:
-            return deconv
+def residual_block(x, output_channels, scope=None, activation=None):
+    with tf.variable_scope(scope or "residual_block"):
+        shortcut = x
+        input_channels = x.get_shape()[-1]
 
-class BatchNorm(object):
+        x = conv2d(x=x,
+                   output_channels=output_channels,
+                   scope='conv_1',
+                   kernel=(3, 3),
+                   stride=1,
+                   activation=activation)
 
-    def __init__(self, epsilon=1e-5, momentum=0.9, scope=None):
-        with tf.variable_scope(scope or "batch_norm") as scope:
-            self.epsilon = epsilon
-            self.momentum = momentum
-            self.name = scope.name
+        x = conv2d(x=x,
+                   output_channels=output_channels,
+                   scope='conv_2',
+                   kernel=(3, 3),
+                   stride=1,
+                   activation=activation)
 
-    def __call__(self, x, train):
-        return tf.contrib.layers.batch_norm(x, decay=self.momentum, updates_collections=None, epsilon=self.epsilon,
-                                            center=True, scale=True, is_training=train, scope=self.name)
+        if input_channels != output_channels:
+            shortcut = conv2d(x=shortcut,
+                              output_channel=output_channels,
+                              scope='conv_1x1',
+                              kernel=(1, 1),
+                              stride=1,
+                              activation=None)
+
+    return tf.add(shortcut, x)
