b0517ebef641c8e53a59624952cdae23e3685912
Veith Roethlingshoefer <veith21@gmail.com>
2018-03-16 22:22:18 +0100

    Fix checkpoint loading and initialization

--- partygan/session_handler.py
+++ partygan/session_handler.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+
 import tensorflow as tf
+
+from interfaces import Model
 
 
 class SessionHandler(object):
 
     def __init__(self, model, model_name, checkpoint_dir="./checkpoints", logdir="./logs"):
+
+        if not isinstance(model, Model):
+            raise ValueError(f"Model must be of type 'Model', not {type(model)}")
 
         self.model_name = model_name
         self._checkpoint_dir = checkpoint_dir
@@ -18,12 +25,13 @@ class SessionHandler(object):
 
 
     def _raise_on_uninitialized(func):
-        def assert_initialization(self, *args, **kwargs):
-            if (self._session is None or self._saver is None
-            or self._running_model is None or self._summary_writer is None):
+
+        def _assert_initialization(self, *args, **kwargs):
+            if self._session is None or self._saver is None or self._summary_writer is None:
                raise AttributeError("Can not use SessionHandler without active context manager.")
-            return func(*args, **kwargs)
-        return assert_initialization
+            return func(self, *args, **kwargs)
+
+        return _assert_initialization
 
     @property
     @_raise_on_uninitialized
@@ -50,36 +58,35 @@ class SessionHandler(object):
         return str(Path(self._checkpoint_dir) / self.model_name)
 
     @property
-    def logdir(self):
+    def log_dir(self):
         return str(Path(self._logdir) / self.model_name)
 
     def __enter__(self):
-        graph = tf.Graph()
-        session = tf.Session(graph=graph)
-        running_model = self.model(graph=graph, **kwargs)
-        summary_writer = tf.summary.FileWriter(self.log_dir, graph)
-        saver = tf.train.Saver(max_to_keep=5)
+        with tf.Graph().as_default():
+            self.model.initialize()
+            session = tf.Session().__enter__()
+            summary_writer = tf.summary.FileWriter(self.log_dir)
+            saver = tf.train.Saver(max_to_keep=5)
         self._session = session
         self._saver = saver
-        self._running_model = running_model
         self._summary_writer = summary_writer
         return self
 
     def __exit__(self, *args, **kwargs):
-        return self._session.__exit(*args, **kwargs)
+        return self._session.__exit__(*args, **kwargs)
 
     @_raise_on_uninitialized
     def load_weights_or_init(self):
         ckpt = tf.train.get_checkpoint_state(self.checkpoint_dir)
         if ckpt and ckpt.model_checkpoint_path:
             print(f"Loading existing model {self.model_name} from {self.checkpoint_dir}")
-            saver.restore(session, ckpt.model_checkpoint_path)
+            self.saver.restore(self.session, ckpt.model_checkpoint_path)
         else:
             print(f"Initializing new model {self.model_name}")
-            sess.run(tf.global_variables_initializer())
+            self.session.run(tf.global_variables_initializer())
 
 
 if __name__ == "__main__":
     from model import SynethesiaModel
-    with SessionHandler(model=SynethesiaModel, model_name="synethesia") as sess_handler:
+    with SessionHandler(model=SynethesiaModel(64), model_name="synethesia") as sess_handler:
         sess_handler.load_weights_or_init()
