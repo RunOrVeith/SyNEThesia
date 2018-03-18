7b8502699e5ab6ca55d36c568bb4410759c9f358
Veith Roethlingshoefer <veith21@gmail.com>
2018-03-18 23:19:54 +0100

    Make image size a parameter

--- synethesia/model.py
+++ synethesia/model.py
@@ -6,16 +6,22 @@ from interfaces import Model
 
 class SynethesiaModel(Model):
 
-    def __init__(self, feature_dim):
-        # TODO make image size a parameter
+    def __init__(self, feature_dim, img_size=(256, 128)):
+
         self.feature_dim = feature_dim
+        self.img_size = img_size
+
+        self._num_3x3 = int(np.floor(np.log2(self.img_size[0])) - 4)
+        self._channels = 2 ** self._num_3x3
+        self._num_residual = 9
+        self._num_transpose = self._num_3x3
 
         self.sound_feature = None
         self.reproduced_sound = None
         self.base_img = None
         self.generated_img = None
 
-        super(SynethesiaModel, self).__init__()
+        super().__init__()
 
     def initialize(self):
         self._build_model()
@@ -44,21 +50,21 @@ class SynethesiaModel(Model):
     def global_step(self):
         return self._global_step
 
-    def _img_from_sound(self, sound_feature, size=(1024, 512)):
-        feature = self._feature_to_tensor(sound_feature=sound_feature, size=size)
-        base_img = self._load_base_image(size=size)
+    def _img_from_sound(self, sound_feature):
+        feature = self._feature_to_tensor(sound_feature=sound_feature)
+        base_img = self._load_base_image()
         assert base_img.get_shape()[1:3] == feature.get_shape()[1:3], "Rows, Cols do not match"
         img_and_sound = tf.concat([base_img, feature], axis=-1, name="generator_input")
         return img_and_sound, base_img
 
-    def _feature_to_tensor(self, sound_feature, size):
-        tile_size = (1, size[0] * size[1])
+    def _feature_to_tensor(self, sound_feature):
+        tile_size = (1, self.img_size[0] * self.img_size[1])
         tensorized = tf.tile(sound_feature, tile_size)
-        tensorized = tf.reshape(tensor=tensorized, shape=(-1, *size, self.feature_dim))
+        tensorized = tf.reshape(tensor=tensorized, shape=(-1, *self.img_size, self.feature_dim))
         return tensorized
 
-    def _load_base_image(self, size):
-        return tf.placeholder(dtype=tf.float32, shape=[None, *size, 3])
+    def _load_base_image(self):
+        return tf.placeholder(dtype=tf.float32, shape=[None, *self.img_size, 3])
 
     def _build_model(self):
 
@@ -78,14 +84,12 @@ class SynethesiaModel(Model):
             self._summary_op = self._build_summary()
 
     def _build_encoder(self, x):
-        num_3x3 = 4
-        num_residual = 9
-        num_transpose = num_3x3
+        channels = self._channels
         with tf.variable_scope("encoder"):
-            x = conv2d(x=x, output_channels=64, kernel=(7, 7), stride=1, scope="7x7_64")
 
-            channels = 2 ** 6
-            for i in range(7, 7 + num_3x3):
+            x = conv2d(x=x, output_channels=channels, kernel=(7, 7), stride=1, scope=f"7x7_{channels}")
+
+            for i in range(self._num_3x3,  2 * self._num_3x3):
                 channels *= 2
                 x = conv2d(x=x, output_channels=channels,
                            kernel=(3, 3), stride=2,
@@ -94,13 +98,13 @@ class SynethesiaModel(Model):
 
             x = residual_block(x=x, output_channels=channels,
                                activation=tf.nn.relu, scope="residual_0")  # TODO add normalization
-            for i in range(num_residual - 1):
+            for i in range(self._num_residual - 1):
                 x = residual_block(x=x, output_channels=channels, scope=f"residual_{i+1}")
 
             channels //= 2
             x = conv2d_transposed(x=x, output_channels=channels, kernel=(3, 3), stride=2,
                                   scope=f"transposed_{channels}")
-            for i in range(num_transpose - 1):
+            for i in range(self._num_transpose - 1):
                 channels //= 2
                 x = conv2d_transposed(x=x, output_channels=channels,
                                       kernel=(3, 3), stride=2,
@@ -109,16 +113,17 @@ class SynethesiaModel(Model):
 
             x = conv2d(x=x, output_channels=3, kernel=(7, 7), stride=1, activation=tf.nn.relu, scope="7x7_3")
             x = tf.nn.sigmoid(x, name="encoder")  # Scale pixels to [0,1]
+
         return x
 
     def _build_decoder(self, from_img):
-        num_residual = 9
-        with tf.variable_scope("decoder"):
-            y = conv2d(x=from_img, output_channels=64, kernel=(7, 7), stride=1, scope="7x7_64")
 
-            y = residual_block(x=y, output_channels=64, activation=tf.nn.relu, scope="residual_0")
-            for i in range(num_residual):
-                y = residual_block(x=y, output_channels=64, scope=f"residual_{i+1}")
+        with tf.variable_scope("decoder"):
+            y = conv2d(x=from_img, output_channels=self._channels, kernel=(7, 7), stride=1, scope="7x7_64")
+
+            y = residual_block(x=y, output_channels=self._channels, activation=tf.nn.relu, scope="residual_0")
+            for i in range(self._num_residual - 1):
+                y = residual_block(x=y, output_channels=self._channels, scope=f"residual_{i+1}")
 
             y = conv2d(x=y, output_channels=self.feature_dim, kernel=(3, 3), stride=1, scope=f"3x3_{self.feature_dim}")
             y = tf.reduce_mean(y, [1, 2], name="decoder")
@@ -131,7 +136,7 @@ class SynethesiaModel(Model):
             reconstruction_loss = tf.losses.huber_loss(labels=real_sound, predictions=generated_sound,
                                                        delta=1.0, scope="huber_loss")
             _, colorfulness = tf.nn.moments(generated_img, axes=[-1], name="colorfulness")
-            _color_loss = - tf.reduce_sum(colorfulness) / tf.cast(tf.size(colorfulness), tf.float32)
+            _color_loss = - tf.reduce_sum(colorfulness)
             color_loss = tf.identity(_color_loss, name="color_loss")
             tf.losses.add_loss(color_loss)
 
