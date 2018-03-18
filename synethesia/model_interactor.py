9398d681386013938db7665679b29832e3db4473
Veith Roethlingshoefer <veith21@gmail.com>
2018-03-18 22:25:01 +0100

    More descriptive logging

--- synethesia/model_interactor.py
+++ synethesia/model_interactor.py
@@ -15,13 +15,14 @@ class TrainingSession(object):
 
     def __init__(self, model, generate_train_dict):
         self.model = model
-        self.generate_train_feed_dict = generate_train_dict
+        self.generate_train_dict = generate_train_dict
 
     def train(self, model_name, data_provider, learning_rate=0.0001, save_every_n_steps=1000):
-        start_time = time.time()
-
         with SessionHandler(model=self.model, model_name=model_name) as session_handler:
             session_handler.load_weights_or_init()
+            start_time = time.time()
+            step = session_handler.step
+            print(f"{'Resuming' if step > 0 else 'Starting'} to train {model_name}: at step {step}")
 
             for input_feature in data_provider:
                 feed_dict = self.generate_train_dict(input_features=input_feature,
@@ -32,7 +33,6 @@ class TrainingSession(object):
                 if step % save_every_n_steps == 0 and step > 0:
                     session_handler.save(step=step)
                     print(f"Step {step}, time: {time_diff(start_time)}: Saving in {pth}")
-
     # TODO integrate validation
 
 
