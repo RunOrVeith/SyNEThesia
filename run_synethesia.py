a74367d35092a0e58bc5d8e9ea8ca249e8d50be8
Veith Roethlingshoefer <veith21@gmail.com>
2018-03-23 01:18:23 +0100

    CLI switch for train/infer

--- run_synethesia.py
+++ run_synethesia.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python
+import sys
 
 from synethesia import Synethesia
 
+# TODO tf.Flags or argparse
+
+
 if __name__ == "__main__":
 
-    train = True
+    train = bool(sys.argv[1])
     batch_size = 32
     img_size = (256, 128)
     model_name = "loss_tests"
@@ -13,7 +17,7 @@ if __name__ == "__main__":
 
     song_files=["/home/veith/Projects/PartyGAN/data/Bearded Skull - 420 [Hip Hop Instrumental]/audio/soundtrack.mp3",
                 "/home/veith/Projects/PartyGAN/data/Gorillaz - Feel Good Inc. (Official Video)/audio/soundtrack.mp3",
-                #"/home/veith/Projects/PartyGAN/data/Gramatik   Just Jammin/audio/soundtrack.mp3"
+                "/home/veith/Projects/PartyGAN/data/Gramatik   Just Jammin/audio/soundtrack.mp3"
                 ]
 
     synethesia = Synethesia(song_files=song_files, batch_size=batch_size, img_size=img_size)
